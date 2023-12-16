05bc922cf4bd88e86afd3125a837438c72fa7a7a
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-16 16:05:34 -0500

    book author

--- src/data/books.js
+++ src/data/books.js
@@ -2,21 +2,29 @@ export const books = [
     {
          id: 'clean-code',
          title: 'Clean Code: Manual de desarrollo ágil de software',
-         img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg"
+         img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
+         opinion: '',
+         author: 'Robert C. Martin'
     },
     {
          id: 'the-clean-coder',
          title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
-         img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg"
+         img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg",
+         opinion: '',
+         author: 'Robert C. Martin'
     },
     {
-         id: 'javaScript-the-good-parts',
+         id: 'javascript-the-good-parts',
          title: 'JavaScript: The Good Parts',
-         img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg"
+         img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg",
+         opinion: '',
+         author: 'Douglas Crockford'
     },
     {
          id: 'eloquent-javaScript',
          title: 'Eloquent JavaScript: A Modern Introduction to Programming',
-         img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg"
+         img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg",
+         opinion: '',
+         author: 'Marijn Haverbeke'
     }
  ]
