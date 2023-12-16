283eeacbd579fd3dd9451a6a204cc80c297ed76f
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-16 15:59:38 -0500

    404 page

---
+++ src/data/books.js
@@ -0,0 +1,22 @@
+export const books = [
+    {
+         id: 'clean-code',
+         title: 'Clean Code: Manual de desarrollo ágil de software',
+         img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg"
+    },
+    {
+         id: 'the-clean-coder',
+         title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
+         img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg"
+    },
+    {
+         id: 'javaScript-the-good-parts',
+         title: 'JavaScript: The Good Parts',
+         img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg"
+    },
+    {
+         id: 'eloquent-javaScript',
+         title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+         img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg"
+    }
+ ]
